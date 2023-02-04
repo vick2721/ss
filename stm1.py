ae4bebdf1305c67ee156d9b5ced58ed7c88b88ad
vick2721 <88751378+vick2721@users.noreply.github.com>
2023-02-04 16:49:59 +0900

    Update stm1.py

--- stm1.py
+++ stm1.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import requests
+from ba4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+from sklearn.liner_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 from datetime import date, timedelta
 import numpy as np
 import matplotlib.pyplot as plt
